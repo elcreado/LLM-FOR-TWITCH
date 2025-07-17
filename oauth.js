c517f4757d78aa6368ed9b2169bf8d31011c7a21
Elcreado_GG <elcreado321@gmail.com>
2025-07-16 22:51:46 -0500

    Fixed Oauth CARACTERS

--- oauth.js
+++ oauth.js
@@ -4,12 +4,12 @@ dotenv.config();
 import { exchangeCode } from '@twurple/auth';
 import { writeFile } from 'fs/promises';
 
-const { TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET, TWITCH_BROADCASTER_LOGIN } = process.env;
+const { TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET, TWITCH_BROADCASTER_LOGIN, TWITCH_ACCESS_CODE } = process.env;
 
 const clientId = TWITCH_CLIENT_ID;
 const clientSecret = TWITCH_CLIENT_SECRET;
 const redirectUri = 'http://localhost';
-const code = 'vc786fj2v6xftdajfh9z1bi2sp7p7c';
+const code = TWITCH_ACCESS_CODE;
 
 try {
   const tokenData = await exchangeCode(clientId, clientSecret, code, redirectUri);
